5f7e4539c9ee98f9165d09109f8fe169631e88c7
seongbeom <qparanoya@gmail.com>
2021-12-04 12:46:22 +0900

    #1 Hash: 완주하지 못한 자 찾기

---
+++ programmers/src/Hash1.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Hash1 {
+    public static void main(String[] args) {
+
+    }
+
+    // 완주하지 못한 자
+    class Solution {
+        public String solution(String[] participant, String[] completion) {
+            String answer = "";
+            Arrays.sort(participant);
+            Arrays.sort(completion);
+            for (int i = 0; i < participant.length; i++) {
+                if (participant[i].equals(completion[i])) {
+                    if (i == completion.length-1) {
+                        answer = participant[i + 1];
+                        break;
+                    }
+                } else {
+                    answer = participant[i];
+                    break;
+                }
+            }
+            return answer;
+        }
+    }
+}
